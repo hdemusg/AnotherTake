5178a682539bef3d374841dd84f185148f5c4fee
hdemusg <sumedhgarimella@gmail.com>
2022-02-19 20:39:05 -0500

    Audio messages are returned

--- bot.py
+++ bot.py
@@ -6,11 +6,16 @@ app = Flask(__name__)
 
 #this doesn't actually run anything. We are using Cloud Functions to run the actual backend
 @app.route("/", methods=["GET", "POST"])
-def process():
+def process(request):
+    audio = request.values.get('MediaUrl0', None)
+    print(type(audio))
+    if audio == None:
+        #audio = request.values.get('Body', None)
+        audio = "Please send a voice message in order for AnotherTake to work its magic."
     from twilio.twiml.messaging_response import MessagingResponse
     resp = MessagingResponse()
-    resp.message("Welcome to AnotherTake! This bot does not have any musical ability yet, sadly.")
-
+    #resp.message("Welcome to AnotherTake! This bot does not have any musical ability yet, sadly.")
+    resp.message(audio)
     return str(resp)
 
 if __name__ == "__main__":
